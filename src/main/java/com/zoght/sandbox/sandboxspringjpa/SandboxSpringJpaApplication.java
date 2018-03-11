1a0f8b54b1feff6ee0de9383cadc4ed456e9c7f9
Tony Zoght <tony@zoght.com>
2018-03-11 09:20:36 -0400

    initial commit

---
+++ src/main/java/com/zoght/sandbox/sandboxspringjpa/SandboxSpringJpaApplication.java
@@ -0,0 +1,12 @@
+package com.zoght.sandbox.sandboxspringjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandboxSpringJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SandboxSpringJpaApplication.class, args);
+    }
+}
---
+++ src/test/java/com/zoght/sandbox/sandboxspringjpa/SandboxSpringJpaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zoght.sandbox.sandboxspringjpa;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SandboxSpringJpaApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
